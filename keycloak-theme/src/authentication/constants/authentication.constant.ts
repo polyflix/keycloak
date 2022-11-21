d5b0badb310e3f489acb0c6a26b8479f684540b7
croumegous <cyril.roumegous@gmail.com>
2022-11-21 14:32:44 +0100

    chore: change more info link

--- keycloak-theme/src/authentication/constants/authentication.constant.ts
+++ keycloak-theme/src/authentication/constants/authentication.constant.ts
@@ -1,3 +1,3 @@
-export const SHOWCASE_URL = 'http://polyflix-do.pages.dopolytech.fr/showcase/';
+export const SHOWCASE_URL = 'https://about.polyflix.dopolytech.fr/';
 export const TOS_URL =
   'https://github.com/polyflix/polyflix/blob/main/CGU-Polyflix.pdf';
